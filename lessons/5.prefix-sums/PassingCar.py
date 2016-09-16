a94b8bcb0a2849c66d305aa8103fb76538252306
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2016-09-16 21:19:58 +0700

    Add python solution for PassingCar

---
+++ lessons/5.prefix-sums/PassingCar.py
@@ -0,0 +1,20 @@
+# https://codility.com/programmers/lessons/5-prefix_sums/passing_cars/
+# TC:
+# SC:
+# python 2.7.13
+
+def solution(A):
+    N = len(A)
+    numZeroes = 0
+    total = 0
+    for i in range(0, N):
+        if A[i] == 0:
+            numZeroes += 1
+        elif A[i] == 1:
+            total += numZeroes
+            if total > 1000000000:
+                return -1
+    return total
+
+if __name__ == '__main__':
+    assert solution([0, 1, 0, 1, 1]) == 5
